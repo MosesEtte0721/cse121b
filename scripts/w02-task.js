f90f2cecf46011dddd9dca4b6b665313356b4edd
Moses <ett20002@byui.edu>
2023-09-23 14:42:25 +0100

    week02

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -7,7 +7,7 @@ let fullName = "Moses Archibong Ette";
 
 let currentYear = new Date();
 
-let filePath = "../images/mosesette.jpg";
+let profilePicture = "../images/mosesette.jpg";
 
 let nameElement = document.getElementById("name");
 
@@ -27,17 +27,24 @@ nameElement.innerHTML = `<strong>${fullName}</strong>`;
 // display the current time the screen
 yearElement.textContent = currentYear.getFullYear();
 // change the value of src attribute
-imgChange.setAttribute("src", filePath);
+imgChange.setAttribute("src", profilePicture);
 // change the value of alt attribute
-imgChange.setAttribute("alt", `profile picture of ${fullName}`);
+imgChange.setAttribute("alt", `profile image of ${fullName}`);
 
 /* Step 5 - Array */
 let favFood = ["afang soup with Fufu", " Aditan soup with Fufu", " edikang Ikong with Garri", " Jollof Rice", " Asa Iwa", " Ekpang Nkukwo", ];
-// let favFood = [];
 
-let singleFav = "Afang Soup with Fufu";
+let anotherFavoriteFood = "ekpang Iwa";
+
 // add the singleFav to the favFood array
-favFood.push(singleFav);
+favFood.push(anotherFavoriteFood);
+
+// Remove the first element of favFood array
+favFood.shift();
+
+// Remove the last element of the favFood array 
+favFood.pop()
+
 // loops through the favFood array and display to the screen
 favFood.forEach((item) => {
     foodElement.innerHTML += `<li>${item}</li><br>`;
